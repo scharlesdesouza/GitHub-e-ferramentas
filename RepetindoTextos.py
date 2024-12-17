46f5a02391729ba8b6491a687b7c36a762b96973
Scharles De souza <scharlesdesouza@gmail.com>
2024-12-17 09:47:12 -0300

    verificando se recebe número para repetir texto

--- RepetindoTextos.py
+++ RepetindoTextos.py
@@ -1,6 +1,9 @@
 # Solicitar uma string e um número inteiro como entrada. Depois retornar a string a quantidade de vezes informada contento espaçamento entre as strings
 
 string = input("Digite uma String: ")
-numero = int(input("Digite um número interiro: "))
 
-print((string + ' ') * numero)
+try:
+    numero = int(input("Digite um número inteiro: "))
+    print((string + ' ') * numero)
+except ValueError:
+    print("Erro: Você deve digitar um número inteiro válido.")
